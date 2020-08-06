d876eb713cd6843d4e54d63bc65ed9d56a32f0a4
avimehenwal <avi.mehanwal@gmail.com>
2020-08-06 19:59:21 +0200

    feat(vuepress): :art: Add components and fill config

---
+++ docs/docs/.vuepress/components/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+
+  <!-- <form action="https://www.getrevue.co/profile/avimehenwal/add_subscriber"
+  method="post" id="revue-form" name="revue-form"  target="_blank">
+  <div class="revue-form-group">
+    <label for="member_email">Email address</label>
+    <input class="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email">
+  </div>
+  <div class="revue-form-group">
+    <label for="member_first_name">First name <span class="optional">(Optional)</span></label>
+    <input class="revue-form-field" placeholder="First name... (Optional)" type="text" name="member[first_name]" id="member_first_name">
+  </div>
+  <div class="revue-form-group">
+    <label for="member_last_name">Last name <span class="optional">(Optional)</span></label>
+    <input class="revue-form-field" placeholder="Last name... (Optional)" type="text" name="member[last_name]" id="member_last_name">
+  </div>
+  <div class="revue-form-actions">
+    <input type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
+  </div>
+  </form>
+</div> -->
+
+    <div id="news">
+      <h3> {{ message}} </h3>
+      <form
+        :action="actionUrl"
+        method="post"
+        name="revue-form"
+        id="revue-form"
+        class="newsletter-form"
+        target="_blank"
+        rel="noopener"
+      >
+        <input
+          name="member[email]"
+          id="member_email"
+          class="newsletter-input"
+          type="email"
+          placeholder="Email address"
+          required
+        >
+        <input
+          name="member[subscribe]"
+          id="member_submit"
+          class="button newsletter-button"
+          type="submit"
+          value="Subscribe"
+        >
+        <!-- type=reset button -->
+      </form>
+    </div>
+
+    <Vssue :title="this.$page.title" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Subscribe to our Newsletter',
+      actionUrl: 'https://www.getrevue.co/profile/avimehenwal/add_subscriber'
+    }
+  }
+}
+</script>
+
+<style scoped>
+#news {
+    padding: 40px 20px 70px;
+    text-align: center;
+}
+.newsletter-form {
+    position: relative;
+    max-width: 550px;
+    margin: 0 auto;
+    box-sizing: border-box;
+}
+.newsletter-input {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px 80px 10px 20px;
+    height: 50px;
+    border-radius: 50px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+}
+.newsletter-button.button {
+    position: absolute;
+    padding: 4px 20px;
+    margin: 0;
+    height: calc(100% - 8px);
+    right: 4px;
+    top: 4px;
+}
+input.button {
+    padding: 0.75em 2em;
+    border-radius: 2em;
+    display: inline-block;
+    color: #fff;
+    background-color: #4fc08d;
+    transition: all 0.15s ease;
+    box-sizing: border-box;
+    border: 1px solid #4fc08d;
+}
+</style>
--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,58 +1,188 @@
-const { description } = require('../../package')
+require('dotenv').config()
+const { description, name, authors} = require('../../package.json')
+const weburl = 'https://avi-vue-components.netlify.app/'
+
+const extendsNetworks = {
+  email: {
+    sharer: 'mailto:?subject=@title&body=@url%0D%0A%0D%0A@description',
+    type: 'direct',
+    icon: '/email.png',
+  },
+  pinterest: {
+    sharer: 'https://pinterest.com/pin/create/button/?url=@url&media=@media&description=@title',
+    type: 'popup',
+    icon: '/pinterest.png',
+  },
+  linkedin: {
+    sharer:
+      'https://www.linkedin.com/shareArticle?mini=true&url=@url&title=@title&summary=@description',
+    type: 'popup',
+    color: '#1786b1',
+    icon:
+      '<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path d="M910.336 0H113.664A114.005333 114.005333 0 0 0 0 113.664v796.672A114.005333 114.005333 0 0 0 113.664 1024h796.672A114.005333 114.005333 0 0 0 1024 910.336V113.664A114.005333 114.005333 0 0 0 910.336 0zM352.256 796.330667H207.189333V375.466667h145.066667z m-72.021333-477.866667a77.824 77.824 0 0 1-81.237334-74.069333A77.824 77.824 0 0 1 280.234667 170.666667a77.824 77.824 0 0 1 81.237333 73.728 77.824 77.824 0 0 1-81.237333 73.386666z m582.314666 477.866667H716.8v-227.669334c0-46.762667-18.432-93.525333-73.045333-93.525333a84.992 84.992 0 0 0-81.237334 94.549333v226.304h-140.629333V375.466667h141.653333v60.757333a155.989333 155.989333 0 0 1 136.533334-71.338667c60.416 0 163.498667 30.378667 163.498666 194.901334z" /></svg>',
+  }
+}
 
 module.exports = {
-  title: 'Vuepress Docs Boilerplate',
+  title: name,
   description: description,
   head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
+    author: authors.name,
+    // repo: pkg.repository.url,
+    // href="https://github.com/[object Object]" https://github.com/git+https://github.com:avimehenwal/vue-components.git
+    repo: 'avimehenwal/javascript',
+    repoLabel: 'GitHub',
+    editLinks: true,
+    editLinkText: 'Help me improve this page!',
+    lastUpdated: true,
+    logo: '/abucoins.svg',
+    // displayAllHeaders: true,               // Default: false
+    activeHeaderLinks: true,
+    sidebar: 'auto',
+    sidebarDepth: 2,
+    searchPlaceholder: 'Search...',
+    smoothScroll: true,
     nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
+      { text: 'Guide', link: '/guide/', },
+      { text: 'Config', link: '/config/' },
+    ],
+  },
+  plugins: [
+    "@vuepress/plugin-active-header-links",
+    "@vuepress/plugin-last-updated",
+    "@vuepress/plugin-medium-zoom",
+    "@vuepress/plugin-back-to-top",
+    "vuepress-plugin-auto-sidebar",
+    "@vuepress/plugin-nprogress",
+    "reading-progress",
+    "@vuepress/pwa",
+    "img-lazy",
+    ['@dovyp/vuepress-plugin-clipboard-copy', true],
+    ['@vuepress/blog', {
+      // mailchimp email marketting service
+      newsletter: { endpoint: process.env.MailChimp },
+      sitemap: { hostname: weburl },
+      feed: { canonical_base: weburl },
+      comment: {
+        service: 'vssue',
+        // set `platform` rather than `api`
+        platform: 'github',
+        owner: 'avimehenwal',
+        repo: 'vue-components',
+        clientId: process.env.GH_ClientId,
+        clientSecret: process.env.GH_ClientSecret,
+        // OR
+        // service: 'disqus',
+        // shortname: process.env.DISQUS_ShortName,
       },
+      // Calssification and Taxonomy
+      directories: [
+        { id: 'post', dirname: '_posts', path: '/', layout: 'MyIndexPost',
+          itemLayout: 'MyPost',
+          pagination: { lengthPerPage: 2 },
+        },
+        { id: 'guide', dirname: 'guide', path: '/guide', },
+      ],
+    }],
+    [ '@vuepress/google-analytics', { 'ga': process.env.GA } ],
+    ['vuepress-plugin-reading-time', {
+      excludes: ['/about', '/tag/.*']
+    }],
+    // https://mermaid-js.github.io/mermaid/#/mermaidAPI?id=mermaidapi-configuration-defaults
+    [ 'vuepress-plugin-mermaidjs', {
+      // "theme": 'forest',
+      // "theme": 'dark',
+      "theme": "default",
+      // "font-size": "20",
+      gantt: {
+        titleTopMargin:20,
+        barHeight:30,
+        barGap:10,
+        topPadding:50,
+        leftPadding:0,
+        gridLineStartPadding:10,
+        fontSize:25,
+        fontFamily:'"Open-Sans", "sans-serif"',
+        numberSectionStyles:4,
+        axisFormat:'%Y-%m-%d',
+      }
+    }],
+    [
+      '@vuepress/register-components',
       {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
+        // componentsDir: `../../node_modules/${pkg.name}`
+        // componentsDir: `${__dirname}/../../../Apps/withjestpreset/src/components`
       }
     ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
-      ],
+    [
+      'vuepress-plugin-mathjax',
+      {
+        target: 'svg',
+        macros: {
+          '*': '\\times',
+        },
+      },
+    ],
+    [
+      'social-share',
+      {
+        networks: ['twitter', 'facebook', 'reddit', 'telegram', 'whatsapp'],
+        email: 'avi.mehanwal@gmail.com',
+        twitterUser: 'avimehenwal',
+        fallbackImage: '/hero.png',
+        autoQuote: true,
+        isPlain: false,
+        extendsNetworks,
+      }
+    ],
+    [
+      'vuepress-plugin-container',
+      {
+        type: 'theorem',
+        defaultTitle: '',
+        before: info => `<div class="theorem"><p class="title">${info}</p>`,
+        after: '</div>',
+      },
+    ],
+    [
+      'vuepress-plugin-container',
+      {
+        type: 'right',
+        defaultTitle: '',
+      },
+    ],
+    [ // DOESNT WORK, want to put title(author name) after main content
+      'vuepress-plugin-container',
+      {
+        type: 'quote',
+        defaultTitle: 'Anonymous',
+        before: info => `<div class="quote">`,
+        after: info => `<p class="title">${info}</p></div>`,
+      },
+    ]
+  ],
+  markdown: {
+    // options for markdown-it-anchor
+    // anchor: { permalink: false },
+    // options for markdown-it-toc
+    // toc: { includeLevel: [1, 2] },
+    linkify: true,                // convert markdown link texts to links
+    extendMarkdown: md => {
+      md.use(require('markdown-it-container'))
+      md.use(require('markdown-it-footnote'))
+      md.use(require('markdown-it-deflist'))
+      md.use(require('markdown-it-emoji'))
+      md.use(require('markdown-it-abbr'))
+      md.use(require('markdown-it-mark'))
+      md.use(require('markdown-it-sup'))
+      md.use(require('markdown-it-sub'))
+      md.use(require('markdown-it-ins'))
     }
-  },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
+  }
 }
