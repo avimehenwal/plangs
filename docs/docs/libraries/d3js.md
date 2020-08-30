7395a592c49edc71ea4a5ce137caeb29b4a63bfb
avimehenwal <avi.mehanwal@gmail.com>
2020-08-30 18:22:37 +0200

    feat(core): :tada: bar graph using div, css and d3

--- docs/docs/libraries/d3js.md
+++ docs/docs/libraries/d3js.md
@@ -21,9 +21,16 @@ $$D3 = Data \, Driven \, Documents$$
 * `append()` add all those missing elements
 * general library for  manupulating docuemnts, text, numers, charts, shapes
 
+![append html elements inside DOM](../.vuepress/public/img/browser/d3-append.png)
+
+## Questions
+
+* [loading external scripts in head vs body?](https://stackoverflow.com/questions/6303490/whats-the-difference-between-html-head-and-body-tags#:~:text=The%20holds%20stuff%20like,oriented%20stuff%2C%20usually%20HTML%20based.)
+
+
 ## References
 
 * https://d3js.org/
-* 
+*
 
 <Footer />
--- src/d3-webpack/app.js
+++ src/d3-webpack/app.js
@@ -1,6 +1,61 @@
-console.log('from app.js');
-
 import * as d3 from 'd3'
 
+console.log('from app.js');
+// #keys in d3 object
+// console.log(Object.keys(d3).length);    // 528
+
+/**
+ * 1. select all SVG rectangle
+ * 2. then make them orange
+ */
 const square = d3.selectAll("rect");
 square.style("fill", "orange");
+
+/**
+ * Render numbers
+ */
+function d3Numbers() {
+  d3.select('#num')
+    .selectAll('p')
+    .data([1, 2, 3])
+    .enter()
+    .append('p')
+    .text(data => data);
+}
+
+function d32(data) {
+  d3.select('#txt')
+    .selectAll('p')
+    .data(data)
+    .enter()
+    .append('p')
+    .text(data => data.region);
+}
+
+/**
+ * Render a bar chart inside a div
+ */
+function d3Bar(data) {
+  d3.select('#graph')
+    .classed('container', true)
+    .style('border', '2px solid red')
+    .selectAll('.bar')
+    .data(data)
+    .enter()
+    .append('div')
+    .classed('bar', true)
+    .style('width', '50px')
+    .style('height', data => (data.value * 15) + 'px')
+}
+
+
+// MAIN
+const DUMMY_DATA = [
+  { id: 1, value: 12, region: 'USA' },
+  { id: 2, value: 10, region: 'Germany' },
+  { id: 3, value: 4, region: 'India' },
+  { id: 3, value: 8, region: 'China' },
+]
+d3Numbers()
+d32(DUMMY_DATA)
+d3Bar(DUMMY_DATA)
