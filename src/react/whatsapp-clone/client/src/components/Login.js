bba75613e0cef446ea7a722f0960556ba857ccbf
avimehenwal <avi.mehanwal@gmail.com>
2020-09-06 14:45:37 +0200

    style(whatsappClone): :zap: place login form in vertical centre of screen

--- src/react/whatsapp-clone/client/src/components/Login.js
+++ src/react/whatsapp-clone/client/src/components/Login.js
@@ -1,17 +1,23 @@
 import React, {useRef} from 'react'
-import { Container, Form } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 
 export default function Login() {
   const idRef = useRef()
 
+  function handleSubmit(e) {
+    // prevent page from refreshing
+    e.preventDefault()
+  }
+
   return (
-    <Container>
-      <Form>
+    <Container className="align-items-center d-flex" style={{ height: '100vh'}}>
+      <Form onSubmit={handleSubmit} className="w-100">
         <Form.Group>
           <Form.Label>Enter your ID</Form.Label>
           <Form.Control type="text" ref={idRef} required></Form.Control>
         </Form.Group>
-        Hello Login
+        <Button type="submit" className="mr-2">Login</Button>
+        <Button variant="secondary">Create New Id</Button>
       </Form>
     </Container>
   )
