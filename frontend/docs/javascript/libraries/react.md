6e7584104e91bcfec32900bd69b3051869793e1c
avimehenwal <avi.mehanwal@gmail.com>
2020-09-06 14:36:58 +0200

    feat(whatsappClone): :lipstick: use bootstrap Container and Form

--- frontend/docs/javascript/libraries/react.md
+++ frontend/docs/javascript/libraries/react.md
@@ -29,6 +29,24 @@ packages | Description
 react-dom     | |
 react-scripts | |
 
+## React Hooks
+
+
+### useRef Hook use case
+
+* How to show how many times a component rendered on screen?
+* Reference elements inside HTML
+  * same effect as document.querySelector()
+
+::: danger useRef
+Do not manage states yourself, let react do it for you. Do not misuse userRef
+Update states using JSX
+:::
+
+
+ref vs state? Difference?
+:   Refs doent cause component to reupdate.
+
 
 ## Features
 
--- src/react/whatsapp-clone/client/src/components/Login.js
+++ src/react/whatsapp-clone/client/src/components/Login.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { Container, Form } from "react-bootstrap";
 
 export default function Login() {
-  return (
-    <div>
+  const idRef = useRef()
 
-    </div>
+  return (
+    <Container>
+      <Form>
+        <Form.Group>
+          <Form.Label>Enter your ID</Form.Label>
+          <Form.Control type="text" ref={idRef} required></Form.Control>
+        </Form.Group>
+        Hello Login
+      </Form>
+    </Container>
   )
 }
